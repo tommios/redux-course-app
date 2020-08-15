38583c7b847958a75fd166c722976c070cc12625
tommios <tommios.net@gmail.com>
2020-08-15 23:18:24 +0300

    add custom store and createStore

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import "./styles.css";
+import { createStore } from "./createStore";
 
 const counter = document.getElementById("counter");
 const addBtn = document.getElementById("add");
 const subBtn = document.getElementById("sub");
 const asyncBtn = document.getElementById("async");
 const themeBtn = document.getElementById("theme");
+
+const store = createStore();
 
 addBtn.addEventListener("click", () => {});
 
